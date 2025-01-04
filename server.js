f6cf913806249a0c271991fcdda52326a9b930fc
JAY CHOKSI <jaychokxii1804@gmail.com>
2025-01-03 21:52:17 -0500

    Added backend logic for serving blog posts and handling dynamic routes

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const port = 3030;
 
 // Middleware for static type files such as css and js file.
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Temporary storage for blog posts
+let blogPosts = [
+  {
+    id: 1,
+    title: "Blog Post Title 1",
+    content: "This is the content of blog post 1.",
+  },
+  {
+    id: 2,
+    title: "Blog Post Title 2",
+    content: "This is the content of blog post 2.",
+  },
+];
+
+// CODE FOR ROUTE
 
 // Now add the routing such as home, blog, etc. By using the routes as slashes.
 app.get("/", (req, res) => {
@@ -16,7 +34,24 @@ app.get("/blogs", (req, res) => {
 });
 
 app.get("/blog/:id", (req, res) => {
-  res.sendFile(__dirname + "/views/blog-post.html");
+  const blogPost = blogPosts.find(
+    (post) => post.id === parseInt(req.params.id)
+  );
+  if (blogPost) {
+    res.send(`
+      <html>
+        <head>
+          <title>${blogPost.title}</title>
+        </head>
+        <body>
+          <h1>${blogPost.title}</h1>
+          <p>${blogPost.content}</p>
+        </body>
+      </html>
+    `);
+  } else {
+    res.status(404).send("Blog post not found");
+  }
 });
 
 // Start server
